0fec977f9430a8e983ed488697c69fdfc50dafe4
Medhavi Basera <1905781@kiit.ac.in>
2023-04-08 12:45:32 +0530

    Router changed to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Link, BrowserRouter } from "react-router-dom";
+import { Link, HashRouter, BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Routes, Route } from "react-router-dom";
 import Feedback from "./components/Feedback";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/Blumea-UI" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/feedback" element={<Feedback />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
