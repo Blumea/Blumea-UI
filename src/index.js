d695f381841d9bb57acaf373c9964debac8f65d2
Medhavi Basera <1905781@kiit.ac.in>
2023-04-18 22:05:43 +0530

    Revamping the navbar :art:

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import { Link, HashRouter, BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Routes, Route } from "react-router-dom";
 import Feedback from "./components/Feedback";
+import UseCase from "./hoc/UseCase";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/features" element={<UseCase />} />
         <Route path="/feedback" element={<Feedback />} />
       </Routes>
     </HashRouter>
